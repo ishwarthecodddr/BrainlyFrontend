3b0c1a2c8e146b8adb759686a8d086290e20d681
ishwarthecodddr <ishwarthecoder@gmail.com>
2025-02-12 20:24:47 +0530

    fixed backend_url

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const API_CONFIG = {
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  },
-  credentials: 'include',
-};
+export const BACKEND_URL="https://brainlybackend-1.onrender.com/"
+// export const API_CONFIG = {
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//   },
+//   credentials: "include",
+// };
